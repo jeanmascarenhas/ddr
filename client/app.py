a97350bdef57e191eb17a31c0a415dd6abed5727
Matheus Gomes <matheus.fig.gomes.935@gmail.com>
2026-09-08 21:41:19 -0300

    chainlit working with fake chat

--- client/app.py
+++ client/app.py
@@ -1,8 +1,80 @@
+import logging
+
 import chainlit as cl
+import httpx
+
+BACKEND_URL = "http://localhost:8000"
 
 
+# @cl.on_message
+# async def on_message(message: cl.Message):
+#     await cl.Message(content=f"Você disse: {message.content}").send()
+
+
+## Request to be used in final version
+# @cl.on_message
+# async def on_message(message: cl.Message):
+
+#     async with httpx.AsyncClient() as client:
+#         response = await client.post(
+#             f"{BACKEND_URL}/chat",
+#             json={"message": message.content},
+#         )
+#         data = response.json()
+#         await cl.Message(content=data["response"]).send()
+
+
+
+# ##Fake request to be used in first version
+# @cl.on_message
+# async def on_message(message: cl.Message):
+#     async with httpx.AsyncClient() as client:
+#         response = await client.post(
+#             f"{BACKEND_URL}/chat/fake",
+#             json={"prompt": message.content},
+#         )
+#         data = response.json()
+#         await cl.Message(content=data["response"]).send()
+
+# ##Fake request to be used in second version
+# @cl.on_message
+# async def on_message(message: cl.Message):
+#     msg = cl.Message(content="")
+#     await msg.send()
+
+#     async with httpx.AsyncClient() as client:
+
+#         async with client.stream(
+#             "POST",
+#             f"{BACKEND_URL}/chat/fake",
+#             json={"prompt": message.content},
+#         ) as response:
+#             async for line in response.aiter_lines():
+#                 if line:
+#                     await msg.stream_token(line)
+
+#     await msg.update()
+
+##Fake request to be used in third version
 @cl.on_message
-async def main(message: cl.Message):
-    await cl.Message(
-        content=f"Você disse: {message.content}"
-    ).send()
+async def on_message(message: cl.Message):
+    msg = cl.Message(content="")
+    await msg.send()
+
+    try:
+        async with httpx.AsyncClient() as client:
+
+            async with client.stream(
+                "POST",
+                f"{BACKEND_URL}/chat/fake",
+                json={"prompt": message.content},
+            ) as response:
+                async for chunk in response.aiter_bytes():
+                    if chunk:
+                        await msg.stream_token(chunk.decode("utf-8"))
+                                        
+
+        await msg.update()
+
+    except httpx.RequestError as e:
+        logging.error(f"Error occurred while making the request: {e}")
--- ddr_api/app/routers/chat.py
+++ ddr_api/app/routers/chat.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
 from langchain_core.messages import HumanMessage
+import asyncio
 
 from app.models.chat_models import (
     ChatRequest,
@@ -33,3 +35,35 @@ async def chat(request: ChatRequest):
         )
     except Exception as e:
             return f"Error {e}"
+
+
+@router.post(
+     "/fake",
+     response_model=ChatResponse, tags=["Fake"]
+)
+async def fake_chat(request: ChatRequest):
+
+    full_text =f"""Fake response for prompt: {request.prompt} +\n
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        accusantium doloremque laudantium, totam rem aperiam, eaque 
+                        ipsa quae ab illo inventore veritatis et quasi architecto  
+                        beatae vitae dicta sunt explicabo. Nemo enim ipsam 
+                        voluptatem quia voluptas sit aspernatur aut odit aut 
+                        fugit, sed quia consequuntur magni dolores eos qui 
+                        ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
+                        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci 
+                        velit, sed quia non numquam eius modi tempora incidunt ut 
+                        labore et dolore magnam aliquam quaerat voluptatem. Ut enim 
+                        ad minima veniam, quis nostrum exercitationem ullam corporis 
+                        suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+                        Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
+                        esse quam nihil molestiae consequatur, vel illum qui dolorem eum 
+                        fugiat quo voluptas nulla pariatur?"""
+
+    async def generate():
+         words = full_text.split(" ")
+         for word in words:
+             yield word + " "
+            #  await asyncio.sleep(0.002)
+
+    return StreamingResponse(generate(), media_type="text/plain")
